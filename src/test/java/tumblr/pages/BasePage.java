b26ec91ca2a4eb8c58527a9aeaad8b6936c41e45
bilyardvmetro <polenovkirill@gmail.com>
2026-03-22 22:15:07 +0300

    fix for firefox

--- src/test/java/tumblr/pages/BasePage.java
+++ src/test/java/tumblr/pages/BasePage.java
@@ -1,14 +1,13 @@
 package tumblr.pages;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
-import java.util.List;
 
 public abstract class BasePage {
 
@@ -51,56 +50,79 @@ public abstract class BasePage {
         }
     }
 
+    protected void jsClick(WebElement element) {
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", element);
+    }
+
     protected void acceptCookiesIfPresent() {
+        driver.switchTo().defaultContent();
+
         if (acceptCookiesInCurrentContext()) {
             driver.switchTo().defaultContent();
             return;
         }
 
-        List<WebElement> iframes = driver.findElements(By.tagName("iframe"));
-        for (WebElement iframe : iframes) {
-            try {
-                driver.switchTo().defaultContent();
-                driver.switchTo().frame(iframe);
-
-                if (acceptCookiesInCurrentContext()) {
-                    driver.switchTo().defaultContent();
-                    return;
-                }
-            } catch (Exception ignored) {
-            }
-        }
+        acceptCookiesInIframe();
 
         driver.switchTo().defaultContent();
     }
 
     private boolean acceptCookiesInCurrentContext() {
-        WebDriverWait shortWait = new WebDriverWait(driver, Duration.ofSeconds(2));
+        WebDriverWait shortWait = new WebDriverWait(driver, Duration.ofSeconds(3));
 
         By cookieDialog = By.cssSelector(".frame-content .cmp__dialog");
         By agreeButton = By.xpath(
-                "//*[contains(@class,'cmp__dialog')]//button[.//span[normalize-space()='I Agree!'] or normalize-space()='I Agree!']"
+                "//*[contains(@class,'cmp__dialog')]//button[" +
+                        "normalize-space()='I Agree!' " +
+                        "or normalize-space()='Согласен' " +
+                        "or .//*[normalize-space()='I Agree!'] " +
+                        "or .//*[normalize-space()='Согласен']" +
+                        "]"
         );
 
         try {
-            WebElement dialog = shortWait.until(
-                    ExpectedConditions.visibilityOfElementLocated(cookieDialog)
+            shortWait.until(ExpectedConditions.visibilityOfElementLocated(cookieDialog));
+            WebElement button = shortWait.until(ExpectedConditions.presenceOfElementLocated(agreeButton));
+            jsClick(button);
+            shortWait.until(ExpectedConditions.invisibilityOfElementLocated(cookieDialog));
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private boolean acceptCookiesInIframe() {
+        WebDriverWait shortWait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
+        By modalIframe = By.cssSelector(".cmp-components-modal__frame iframe");
+        By cookieDialog = By.cssSelector(".frame-content .cmp__dialog");
+        By agreeButton = By.xpath(
+                "//*[contains(@class,'cmp__dialog')]//button[" +
+                        "normalize-space()='I Agree!' " +
+                        "or normalize-space()='Согласен' " +
+                        "or .//*[normalize-space()='I Agree!'] " +
+                        "or .//*[normalize-space()='Согласен']" +
+                        "]"
+        );
+
+        try {
+            WebElement iframe = shortWait.until(
+                    ExpectedConditions.presenceOfElementLocated(modalIframe)
             );
 
-            WebElement button = dialog.findElement(
-                    By.xpath(".//button[.//span[normalize-space()='I Agree!'] or normalize-space()='I Agree!']")
-            );
+            driver.switchTo().frame(iframe);
 
-            try {
-                button.click();
-            } catch (Exception e) {
-//                jsClick(button);
-            }
+            shortWait.until(ExpectedConditions.visibilityOfElementLocated(cookieDialog));
+            WebElement button = shortWait.until(ExpectedConditions.presenceOfElementLocated(agreeButton));
+
+            jsClick(button);
 
             shortWait.until(ExpectedConditions.invisibilityOfElementLocated(cookieDialog));
             return true;
         } catch (Exception e) {
             return false;
+        } finally {
+            driver.switchTo().defaultContent();
         }
     }
 }
